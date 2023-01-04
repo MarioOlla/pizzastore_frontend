1dd110116cfff1b9f904680e90b62312cc81a3ad
MarioOlla <mario.olla25@gmail.com>
2023-01-04 18:29:55 +0100

    Aggiunti login module e login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  
+  {
+    path: 'login',
+    loadChildren: () => import('./core/auth/login/login.module').then(m => m.LoginModule)
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
